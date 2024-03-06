aa34a7069adf65bd4a1c3d4984d513ce2a5cdbac
cyburns <72774499+cyburns@users.noreply.github.com>
2024-03-06 11:39:46 -0500

    switching 3/6

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,21 +1,34 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import React from "react";
 import { useAuth } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { defaultStyles } from "@/constants/Style";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const Profile = () => {
   const { signOut, isSignedIn } = useAuth();
 
   return (
-    <View>
+    <GestureHandlerRootView style={styles.container}>
       <Button title="Log out" onPress={() => signOut()} />
       {!isSignedIn && (
         <Link href={"/(modals)/login"}>
-          <Text>Log in</Text>
+          <TouchableOpacity style={defaultStyles.btn}>
+            <Text style={defaultStyles.btnText}>Log in</Text>
+          </TouchableOpacity>
         </Link>
       )}
-    </View>
+    </GestureHandlerRootView>
   );
 };
 
 export default Profile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 26,
+  },
+});
