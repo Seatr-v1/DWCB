e2341838fc11c973d522202bcc599206c1029792
cyburns <72774499+cyburns@users.noreply.github.com>
2024-03-18 13:13:21 -0400

    basics of profile done

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -63,9 +63,10 @@ const Profile = () => {
 
   return (
     <GestureHandlerRootView style={defaultStyles.container}>
-      <SafeAreaView style={defaultStyles.container}>
+      <SafeAreaView style={styles.container}>
         <View style={styles.headerContainer}>
           <Text style={styles.header}>Profile</Text>
+
           {edit ? (
             <TouchableOpacity onPress={onSaveUser}>
               <Ionicons
@@ -76,7 +77,7 @@ const Profile = () => {
             </TouchableOpacity>
           ) : (
             <TouchableOpacity onPress={() => setEdit(true)}>
-              <Text style={styles.editText}>Edit</Text>
+              <Text style={styles.editText}>{isSignedIn && "Edit"}</Text>
             </TouchableOpacity>
           )}
         </View>
@@ -89,7 +90,7 @@ const Profile = () => {
             <View style={{ flexDirection: "row", gap: 6 }}>
               {!edit && (
                 <View style={styles.editRow}>
-                  <Text style={{ fontFamily: "mon-sb", fontSize: 22 }}>
+                  <Text style={{ fontFamily: "mon-sb", fontSize: 28 }}>
                     Hi, I'm {firstName}
                   </Text>
                 </View>
@@ -111,22 +112,68 @@ const Profile = () => {
                 </View>
               )}
             </View>
-            <Text style={{ color: Colors.darkGray }}>
+            <Text style={{ color: Colors.darkGray, marginBottom: 10 }}>
               Joined on {user?.createdAt!.toLocaleDateString()}
             </Text>
+
+            <View style={styles.divider} />
           </View>
         )}
+        <View>
+          <Text
+            style={{
+              fontFamily: "mon-sb",
+              fontSize: 28,
+              marginLeft: 24,
+              marginBottom: 8,
+            }}
+          >
+            About
+          </Text>
+          <Text
+            style={{ color: Colors.darkGray, marginLeft: 24, marginBottom: 24 }}
+          >
+            {email}
+          </Text>
+          <View style={styles.card}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <View style={{ flex: 1 }}>
+                <Text
+                  style={{
+                    fontFamily: "mon-sb",
+                    fontSize: 18,
+                    marginBottom: 8,
+                  }}
+                >
+                  Sell reservations
+                </Text>
+                <Text style={{ color: Colors.darkGray }}>
+                  List your reservations and {"\n"}start earning.
+                </Text>
+              </View>
+              <Image
+                source={require("../../assets/images/rest-rend.png")}
+                style={styles.image}
+              />
+            </View>
+          </View>
+        </View>
+
+        <View style={{ padding: 24 }}>
+          <View style={styles.divider} />
+        </View>
 
         {isSignedIn && (
-          <Button
-            title="Log Out"
-            onPress={() => signOut()}
-            color={Colors.darkGray}
-          />
+          <TouchableOpacity style={styles.btnOutline} onPress={() => signOut()}>
+            <Text style={styles.btnOutlineText}>Log out</Text>
+          </TouchableOpacity>
         )}
+
         {!isSignedIn && (
           <Link href={"/(modals)/login"} asChild>
-            <Button title="Log In" color={Colors.darkGray} />
+            <TouchableOpacity style={styles.btn}>
+              <Text style={styles.btnText}>Log in</Text>
+            </TouchableOpacity>
           </Link>
         )}
       </SafeAreaView>
@@ -139,6 +186,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     padding: 24,
+  },
+  container: {
+    padding: 5,
   },
   header: {
     fontFamily: "mon-sb",
@@ -154,10 +204,9 @@ const styles = StyleSheet.create({
   },
   card: {
     backgroundColor: "#fff",
-    padding: 24,
+    padding: 14,
     borderRadius: 16,
     marginHorizontal: 24,
-    marginTop: 24,
     elevation: 2,
     shadowColor: "#000",
     shadowOpacity: 0.2,
@@ -166,9 +215,8 @@ const styles = StyleSheet.create({
       width: 1,
       height: 2,
     },
-    alignItems: "center",
     gap: 14,
-    marginBottom: 24,
+    marginBottom: 5,
   },
   avatar: {
     width: 100,
@@ -187,6 +235,44 @@ const styles = StyleSheet.create({
     textDecorationLine: "underline",
     textDecorationStyle: "solid",
     textDecorationColor: "#000",
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: Colors.darkGray,
+    marginVertical: 5,
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    height: 50,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 24,
+  },
+  btnText: {
+    color: Colors.white,
+    fontSize: 16,
+    fontFamily: "mon-b",
+  },
+  image: {
+    height: 100,
+    width: 100,
+  },
+  btnOutline: {
+    margin: 24,
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: Colors.darkGray,
+    height: 50,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: "#000",
+    fontSize: 16,
   },
 });
 
