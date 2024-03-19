2618025165983bda99ddfc0f7706f4909f3fef21
cyburns <72774499+cyburns@users.noreply.github.com>
2024-03-19 09:07:37 -0400

    checkout for now

--- app/(modals)/where-to.tsx
+++ app/(modals)/where-to.tsx
@@ -9,11 +9,17 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
 
+const AminatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
+
 const Where = () => {
-  const [openCard, setOpenCard] = useState(false);
-  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [openCard, setOpenCard] = useState(0);
+  const [selectedPlace, setSelectedPlace] = useState(0);
   const router = useRouter();
-  const handleClearAll = () => {};
+  const handleClearAll = () => {
+    setSelectedPlace(0);
+    setOpenCard(0);
+  };
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
@@ -68,14 +74,89 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 100,
   },
-  footerText: {
-    height: "100%",
-    flexDirection: "column",
-    gap: 4,
+  card: {
+    backgroundColor: Colors.white,
+    borderRadius: 14,
+    margin: 10,
+    elevation: 4,
+    shadowColor: Colors.black,
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    gap: 20,
   },
-  footerPrice: {
-    fontSize: 18,
+  cardHeader: {
+    fontFamily: "mon-b",
+    fontSize: 24,
+    padding: 20,
+  },
+  cardBody: {
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+  cardPreview: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  searchSection: {
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.white,
+    borderWidth: 1,
+    borderColor: "#ABABAB",
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  searchIcon: {
+    padding: 10,
+  },
+  inputField: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: Colors.white,
+  },
+  placesContainer: {
+    flexDirection: "row",
+    gap: 25,
+  },
+  place: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  placeSelected: {
+    borderColor: Colors.darkGray,
+    borderWidth: 2,
+    borderRadius: 10,
+    width: 100,
+    height: 100,
+  },
+  previewText: {
     fontFamily: "mon-sb",
+    fontSize: 14,
+    color: Colors.darkGray,
+  },
+  previewdData: {
+    fontFamily: "mon-sb",
+    fontSize: 14,
+    color: Colors.darkGray,
+  },
+
+  guestItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 16,
+  },
+  itemBorder: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: Colors.darkGray,
   },
 });
 
